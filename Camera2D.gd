1a4065a0550fa02d2db58ff505a85bb4539896bf
Fernando Muñoz <shaaf4ever@gmail.com>
2025-08-20 02:06:19 -0600

    Update Camera2D.gd

--- Camera2D.gd
+++ Camera2D.gd
@@ -1,16 +1,18 @@
 extends Camera2D
 
 var t
-func shake(qty = 8, duration = 0.2):
+func shake(qty = 8, duration = 0.15):
 	if t:
 		t.kill()
 	t = create_tween()
-	shake_qty = qty
-	t.tween_property(self, 'shake_qty', 0, duration).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+	_shake_qty = qty
+	t.tween_property(self, '_shake_qty', 0, duration).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 
-var shake_qty = 0
+var _shake_qty = 0
 func _process(delta: float) -> void:
-	if shake_qty:
+	if _shake_qty:
 		var a = randf() * PI * 2
 		var dir = Vector2(cos(a), sin(a))
-		offset = shake_qty * dir * randf()
+		offset = _shake_qty * dir * randf()
+	else:
+		offset = Vector2.ZERO
