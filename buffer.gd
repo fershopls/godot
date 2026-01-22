5609640dad2984a39a99826ca42e42185ef64c72
Fernando Muñoz <shaaf4ever@gmail.com>
2026-01-21 19:17:30 -0600

    Update buffer.gd

--- buffer.gd
+++ buffer.gd
@@ -1,17 +1,25 @@
-extends Timer
+extends Object
 class_name Buffer
 
-var ttl: int
+"""
+# Firerate example:
+var _buffer = Buffer.new(400)
+if not _buffer.is_queued():
+	_buffer.queue()
+	# attack()
+"""
+
+var ttl_ms: int
 var queued_at: int
 
-func _init(ttl: int = 60):
-	self.ttl = ttl
+func _init(ttl_ms: int = 60):
+	self.ttl_ms = ttl_ms
 
 func queue():
 	queued_at = _now()
 
 func is_queued():
-	return _now() - queued_at <= ttl
+	return _now() - queued_at <= ttl_ms
 
 func clear():
 	queued_at = 0
