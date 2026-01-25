a51ae8fed708c9b3694b487b38373f5c5a75ebab
Fernando Muñoz <shaaf4ever@gmail.com>
2026-01-24 20:15:21 -0600

    Update and rename utils/debug.gd to debug.gd

--- debug.gd
+++ debug.gd
@@ -54,7 +54,7 @@ func screenshot():
 	if dir.dir_exists('assets'):
 		path = 'res://assets/SCREENSHOT_'
 	
-	image.save_png(path + str(Kit.timestamp()) + '.png')
+	image.save_png(path + str(timestamp()) + '.png')
 
 var track_data: = {}
 var track_label
@@ -68,19 +68,22 @@ func track(values):
 	track_data.merge(values, true)
 
 func _create_track_label():
-	var m = Kit.instantiate(MarginContainer.new(), self)
-	var p: Panel = Kit.instantiate(Panel.new(), m)
+	var m = instantiate(MarginContainer.new(), self)
+	var p: Panel = instantiate(Panel.new(), m)
 	var bg = StyleBoxFlat.new()
 	bg.bg_color = Color.BLACK
 	p.add_theme_stylebox_override('panel', bg)
-	var l: Label = Kit.instantiate(Label.new(), m)
+	var l: Label = instantiate(Label.new(), m)
 	l.add_theme_font_size_override('font_size', font_size)
 	return l
 
 func _track_get_value(value) -> String:
 	# Vector2
-	if typeof(value) == TYPE_VECTOR2 or typeof(value) == TYPE_VECTOR2I:
+	if typeof(value) == TYPE_VECTOR2:
 		value = value.floor()
+		return str(value.x)+', '+str(value.y)
+	
+	if typeof(value) == TYPE_VECTOR2I:
 		return str(value.x)+', '+str(value.y)
 	
 	# String
@@ -95,3 +98,19 @@ func _process(delta):
 		for key in track_data:
 			text += key+": "+_track_get_value(track_data[key])+"\n"
 		track_label.text = text
+
+static func instantiate(node, parent: Node, at = null):
+	if typeof(node) == TYPE_STRING:
+		node = load(node).instantiate()
+	elif node is PackedScene:
+		node = node.instantiate()
+	
+	parent.add_child(node)
+	
+	if at:
+		node.global_position = at
+	
+	return node
+
+static func timestamp():
+	return int(Time.get_unix_time_from_system())
